9cdde972ff0c05f73a65ce5c0b7531ab1efb6681
Sanctum <lukas.manus@t-online.de>
2018-09-20 09:26:10 +0200

    Connector fixed

--- sources/Connect.java
+++ sources/Connect.java
@@ -1,24 +1,28 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Connect {
 
-	public static void main(String[] args) {
-		//Connect(){
+	Connect(){
 		try {
-			//Class.forName("com.mysql.jdbc.Driver").newInstance();
-			Connection connection = DriverManager.getConnection("jdbc:mysql://169.254.207.248:3306/zitatedb", "Lukas", "");
+			Connection connection = DriverManager.getConnection("jdbc:mysql://169.254.207.248:3306/zitatedb?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=CET", "Lukas", "root");
 			System.out.println("Connection successfull!");
+			/*Statement myStmt = connection.createStatement();
+			ResultSet myRs = myStmt.executeQuery("SELECT * FROM tbluser");
+			while(myRs.next()) {
+				System.out.println(myRs.getString("vorname") + "," + myRs.getString("nachname"));
+			}*/
 		}
 		catch(Exception e){
 			System.out.println(e);
 		}
-	//}
 	}
 	
 }
