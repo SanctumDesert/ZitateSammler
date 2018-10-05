3ff709a640299cc3cc2445d110515eaf43b6eca3
Sanctum <lukas.manus@t-online.de>
2018-10-05 07:32:43 +0200

    Update Registration

--- sources/Registrieren.java
+++ sources/Registrieren.java
@@ -6,16 +6,22 @@ import javax.swing.SwingConstants;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.awt.event.ActionEvent;
 
 public class Registrieren {
 
 	private JFrame frame;
-	private JPasswordField passwordField;
-	private JPasswordField passwordField_1;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
+	private JPasswordField txtPassword;
+	private JPasswordField txtPasswordRepeat;
+	private JTextField txtUsername;
+	private JTextField txtFirstName;
+	private JTextField txtLastName;
+	private JTextField txtMail;
 
 	/**
 	 * Launch the application.
@@ -44,6 +50,7 @@ public class Registrieren {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		Connect conn = new Connect();
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 394);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -76,38 +83,145 @@ public class Registrieren {
 		
 		JLabel lblPasswortWiederholen = new JLabel("Passwort wiederholen:");
 		lblPasswortWiederholen.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblPasswortWiederholen.setBounds(39, 222, 109, 14);
+		lblPasswortWiederholen.setBounds(17, 221, 131, 14);
 		frame.getContentPane().add(lblPasswortWiederholen);
 		
-		passwordField = new JPasswordField();
-		passwordField.setBounds(156, 194, 148, 17);
-		frame.getContentPane().add(passwordField);
+		txtPassword = new JPasswordField();
+		txtPassword.setBounds(156, 194, 148, 17);
+		frame.getContentPane().add(txtPassword);
 		
-		passwordField_1 = new JPasswordField();
-		passwordField_1.setBounds(156, 219, 148, 17);
-		frame.getContentPane().add(passwordField_1);
+		txtPasswordRepeat = new JPasswordField();
+		txtPasswordRepeat.setBounds(156, 219, 148, 17);
+		frame.getContentPane().add(txtPasswordRepeat);
 		
-		textField = new JTextField();
-		textField.setBounds(156, 50, 148, 20);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
+		txtUsername = new JTextField();
+		txtUsername.setBounds(156, 50, 148, 20);
+		frame.getContentPane().add(txtUsername);
+		txtUsername.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(156, 85, 148, 20);
-		frame.getContentPane().add(textField_1);
+		txtFirstName = new JTextField();
+		txtFirstName.setColumns(10);
+		txtFirstName.setBounds(156, 85, 148, 20);
+		frame.getContentPane().add(txtFirstName);
 		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(156, 118, 148, 20);
-		frame.getContentPane().add(textField_2);
+		txtLastName = new JTextField();
+		txtLastName.setColumns(10);
+		txtLastName.setBounds(156, 118, 148, 20);
+		frame.getContentPane().add(txtLastName);
 		
-		textField_3 = new JTextField();
-		textField_3.setColumns(10);
-		textField_3.setBounds(156, 149, 148, 20);
-		frame.getContentPane().add(textField_3);
+		txtMail = new JTextField();
+		txtMail.setColumns(10);
+		txtMail.setBounds(156, 149, 148, 20);
+		frame.getContentPane().add(txtMail);
 		
 		JButton btnRegister = new JButton("Registrieren");
+		btnRegister.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				boolean validInput = true;
+				//Check if all fields are set.
+				if(txtUsername.getText()==null) {
+					System.out.println("Bitte gib einen Nutzernamen ein.");
+					validInput = false;
+				}
+				else {
+					PreparedStatement myStmt;
+					try {
+						myStmt = conn.getConnection().prepareStatement("SELECT * FROM tbluser WHERE nutzername=?");
+						myStmt.setString(1, txtUsername.getText());
+						ResultSet myRs = myStmt.executeQuery();
+						if(myRs.next()) {
+							System.out.println("Dieser Nutzername ist bereits vergeben.");
+							validInput = false;
+						}
+						else {
+							System.out.println("Dieser Nutzername ist frei.");
+						}
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				if(txtFirstName.getText()==null) {
+					System.out.println("Bitte gib einen Vornamen ein.");
+					validInput = false;
+				}
+				if(txtLastName.getText()==null) {
+					System.out.println("Bitte gib einen Nachnamen ein.");
+					validInput = false;
+				}
+				if(txtMail.getText()==null) {
+					System.out.println("Bitte gib eine Mailadresse ein.");
+					validInput = false;
+				}
+				else {
+					PreparedStatement myStmt;
+					try {
+						myStmt = conn.getConnection().prepareStatement("SELECT * FROM tbluser WHERE mail=?");
+						myStmt.setString(1, txtMail.getText());
+						ResultSet myRs = myStmt.executeQuery();
+						if(myRs.next()) {
+							System.out.println("Diese Mailadresse wird bereits verwendet.");
+							validInput = false;
+						}
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				if(txtPassword.getPassword()==null) {
+					System.out.println("Bitte gib ein Passwort ein.");
+					validInput = false;
+				}
+				if(txtPasswordRepeat.getPassword()==null) {
+					System.out.println("Bitte wiederhole dein Passwort.");
+					validInput = false;
+				}
+				if(!Arrays.equals(txtPassword.getPassword(),txtPasswordRepeat.getPassword())) {
+					System.out.println("Die Passwörter stimmen nicht überein.");
+					validInput = false;
+				}
+				if(txtPassword.getPassword().length<6) {
+					System.out.println("Das Passwort muss mindestens 6 Zeichen lang sein.");
+					validInput = false;
+				}
+				boolean uppercase=false;
+				boolean lowercase=false;
+				boolean special=false;
+				char[] password = txtPassword.getPassword();
+				for(int i=0; i < password.length; i++) {
+					if(password[i]>='A' && password[i]<='Z') {
+						uppercase=true;
+					}
+					if(password[i]>='a' && password[i]<='z') {
+						lowercase=true;
+					}
+					if(password[i]=='€' || password[i]=='!' || password[i]=='§' || password[i]=='$' || password[i]=='%' || password[i]=='&') {
+						special=true;
+					}
+					if(uppercase==true && lowercase==true && special == true) {
+						break;
+					}
+				}
+				if(uppercase==false) {
+					System.out.println("Das Passwort muss mindestens einen Kleinbuchstaben enthalten.");
+					validInput = false;
+				}
+				if(lowercase==false) {
+					System.out.println("Das passwort muss mindestens einen Großbuchstaben enthalten.");
+					validInput=false;
+				}
+				if(special==false) {
+					System.out.println("Das Passowrt muss mindestens ein gültiges Sonderzeichen enthalten.");
+					System.out.println("Gültige Sonderzeichen sind €, !, §, $, %, &");
+					validInput=false;
+				}
+				
+				
+				
+				
+			}
+		});
 		btnRegister.setBounds(69, 292, 114, 23);
 		frame.getContentPane().add(btnRegister);
 		
