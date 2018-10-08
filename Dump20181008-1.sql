78f3cce3b61130e13d10cac96585c0cee3a37e5d
CyberKuro <CyberKuro@users.noreply.github.com>
2018-10-08 13:58:13 +0200

    Faildumpweg_neuerDump

--- Dump20181008-1.sql
+++ Dump20181008-1.sql
@@ -18,6 +18,23 @@ USE `zitatedb`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `tblaenderungen`
+--
+
+DROP TABLE IF EXISTS `tblaenderungen`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tblaenderungen` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `UserID` varchar(45) NOT NULL,
+  `ZitatID` varchar(45) NOT NULL,
+  `Datum` bigint(30) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `ID_UNIQUE` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Dumping data for table `tblaenderungen`
 --
 
@@ -25,6 +42,22 @@ LOCK TABLES `tblaenderungen` WRITE;
 /*!40000 ALTER TABLE `tblaenderungen` DISABLE KEYS */;
 /*!40000 ALTER TABLE `tblaenderungen` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `tblklassen`
+--
+
+DROP TABLE IF EXISTS `tblklassen`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tblklassen` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Klasse` varchar(45) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `ID_UNIQUE` (`ID`),
+  UNIQUE KEY `Klasse_UNIQUE` (`Klasse`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `tblklassen`
@@ -37,6 +70,23 @@ INSERT INTO `tblklassen` VALUES (1,'12FIAE'),(2,'12FISI');
 UNLOCK TABLES;
 
 --
+-- Table structure for table `tblkurs`
+--
+
+DROP TABLE IF EXISTS `tblkurs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tblkurs` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Lehrer` varchar(45) NOT NULL,
+  `Kurs` varchar(45) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `Kurs_UNIQUE` (`Kurs`),
+  UNIQUE KEY `ID_UNIQUE` (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Dumping data for table `tblkurs`
 --
 
@@ -47,6 +97,28 @@ INSERT INTO `tblkurs` VALUES (1,'Gruening','LF6'),(2,'Schuster','LF5'),(3,'Gruen
 UNLOCK TABLES;
 
 --
+-- Table structure for table `tbluser`
+--
+
+DROP TABLE IF EXISTS `tbluser`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tbluser` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Nutzername` varchar(45) NOT NULL,
+  `Vorname` varchar(45) NOT NULL,
+  `Nachname` varchar(45) NOT NULL,
+  `Passwort` varchar(70) NOT NULL,
+  `Mail` varchar(45) NOT NULL,
+  `Admin` tinyint(4) NOT NULL,
+  `KlasseID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `Nutzername_UNIQUE` (`Nutzername`),
+  UNIQUE KEY `idx_tbluser_Mail` (`Mail`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Dumping data for table `tbluser`
 --
 
@@ -55,6 +127,24 @@ LOCK TABLES `tbluser` WRITE;
 INSERT INTO `tbluser` VALUES (1,'Kuro','Fabian','Minx','9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08','test',1,1),(2,'Sanctum','Lukas','Manus','60303ae22b998861bce3b28f33eec1be758a213c86c93c076dbe9f558c11c752','test2',1,1),(4,'Tester','testVorname','testNachname','testPW','testMail',0,1);
 /*!40000 ALTER TABLE `tbluser` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `tblzitate`
+--
+
+DROP TABLE IF EXISTS `tblzitate`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tblzitate` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `UrheberID` varchar(45) NOT NULL,
+  `SprecherID` varchar(45) NOT NULL,
+  `KursID` varchar(45) NOT NULL,
+  `Datum` bigint(30) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `ID_UNIQUE` (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `tblzitate`
@@ -83,4 +173,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-10-08 13:52:47
+-- Dump completed on 2018-10-08 13:57:26
